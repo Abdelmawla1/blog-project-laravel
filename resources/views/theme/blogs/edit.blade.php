33f2f5c3af1f8ce086ef5b354683658f8b1d5f0f
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-10-23 16:59:21 +0300

    Creating Blog Module

---
+++ resources/views/theme/blogs/edit.blade.php
@@ -0,0 +1,75 @@
+@extends('theme.master')
+@section('title', 'Remake Barber - Create Blog')
+@section('content')
+
+    <!--================ Hero sm banner start =================-->
+    <section class="mb-5px">
+        <div class="container">
+            <div class="hero-banner hero-banner--sm">
+                <div class="hero-banner__content">
+                    <h1>Add new blog</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ Hero sm banner end =================-->
+
+    <!-- ================ blog section start ================= -->
+    <section class="section-margin--small section-margin">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    @if(session('success-blog'))
+                        <div class="alert alert-success">
+                            {{ session('success-blog') }}
+                        </div>
+                    @endif
+
+                    <form action="{{ route('blogs.store') }}" class="form-contact contact_form" method="post"
+                          id="contactForm" novalidate="novalidate" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group">
+                            <input class="form-control border" name="name" id="name" type="text"
+                                   placeholder="Enter your blog title" value="{{ old('name') }}">
+                            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+                        </div>
+
+                        <div class="form-group">
+                            <input class="form-control border" name="image" type="file">
+                            <x-input-error :messages="$errors->get('image')" class="mt-2"/>
+                        </div>
+
+                        <div class="form-group">
+                            <select class="form-control border" name="category_id" value="{{ old('category_id') }}">
+                                <option value="">Select Category</option>
+                                @if(count($categories) > 0)
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                @endif
+                            </select>
+                            <x-input-error :messages="$errors->get('category_id')" class="mt-2"/>
+                        </div>
+
+                        <div class="form-group">
+                            <textarea class="form-control border" name="description" type="text"
+                                      placeholder="Enter your blog content" rows="15">{{ old('description') }}</textarea>
+                            <x-input-error :messages="$errors->get('description')" class="mt-2"/>
+                        </div>
+
+                        <div class="form-group text-center text-md-right mt-3">
+                            <button type="submit" class="button button--active button-contactForm">Create Blog</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+    <!-- ================ contact section end ================= -->
+@endsection
