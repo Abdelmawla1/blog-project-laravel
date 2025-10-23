0b0c3b494061f2c2a4540c1a34ef65dc5fc73095
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-10-23 17:51:25 +0300

    Creating Comment Module

---
+++ app/Models/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = ['name', 'email', 'subject', 'message', 'blog_id'];
+
+    public function blog(): BelongsTo
+    {
+        return $this->belongsTo(Blog::class);
+    }
+}
