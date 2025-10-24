40ce87d957c7eaeb762587cff99bca4951142d39
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-10-24 15:38:44 +0300

    HomePage slider

--- resources/views/theme/index.blade.php
+++ resources/views/theme/index.blade.php
@@ -5,7 +5,7 @@
 
     <main class="site-main">
         <!--================Hero Banner start =================-->
-{{--        @include('theme.partials.hero')--}}
+        {{--        @include('theme.partials.hero')--}}
         <section class="mb-30px">
             <div class="container">
                 <div class="hero-banner">
@@ -18,84 +18,31 @@
             </div>
         </section>
         <!--================ Blog slider start =================-->
-        <section>
-            <div class="container">
-                <div class="owl-carousel owl-theme blog-slider">
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide1.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide2.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide3.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide1.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide2.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-                    <div class="card blog__slide text-center">
-                        <div class="blog__slide__img">
-                            <img class="card-img rounded-0"
-                                 src="{{ asset('assets') }}/img/blog/blog-slider/blog-slide3.png"
-                                 alt="">
-                        </div>
-                        <div class="blog__slide__content">
-                            <a class="blog__slide__label" href="#">Fashion</a>
-                            <h3><a href="#">New york fashion week's continued the evolution</a></h3>
-                            <p>2 days ago</p>
-                        </div>
+        @if(count($sliderBlogs) > 0)
+
+            <section>
+                <div class="container">
+                    <div class="owl-carousel owl-theme blog-slider">
+                        @foreach($sliderBlogs as $blog)
+
+                            <div class="card blog__slide text-center">
+                                <div class="blog__slide__img">
+                                    <img class="card-img rounded-0"
+                                         src="{{ asset("storage/blogs/$blog->image") }}"
+                                         alt="">
+                                </div>
+                                <div class="blog__slide__content">
+                                    <a class="blog__slide__label" href="{{ route('theme.category', ['id' => $blog->category->id]) }}">{{ $blog->category->name }}</a>
+                                    <h3><a href="{{ route('blogs.show', ['blog' => $blog]) }}">{{ $blog->name }}</a></h3>
+                                    <p>{{ $blog->created_at->format('d M') }}</p>
+                                </div>
+                            </div>
+                        @endforeach
+
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        @endif
         <!--================ Blog slider end =================-->
 
         <!--================ Start Blog Post Area =================-->
@@ -107,10 +54,13 @@
                             @foreach($blogs as $blog)
                                 <div class="single-recent-blog-post">
                                     <div class="thumb">
-                                        <img class="img-fluid" src="{{ asset('storage') }}/blogs/{{$blog->image}}" alt="">
+                                        <img class="img-fluid" src="{{ asset('storage') }}/blogs/{{$blog->image}}"
+                                             alt="">
                                         <ul class="thumb-info">
                                             <li><a href="#"><i class="ti-user"></i>{{ $blog->user->name }}</a></li>
-                                            <li><a href="#"><i class="ti-notepad"></i>{{ $blog->created_at->format('D M Y') }}</a></li>
+                                            <li><a href="#"><i
+                                                        class="ti-notepad"></i>{{ $blog->created_at->format('D M Y') }}
+                                                </a></li>
                                             <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
                                         </ul>
                                     </div>
@@ -119,7 +69,8 @@
                                             <h3>{{ $blog->name.'.' }}</h3>
                                         </a>
                                         <p>{{ $blog->description }}</p>
-                                        <a class="button" href="{{ route('blogs.show', ['blog' => $blog]) }}">Read More <i class="ti-arrow-right"></i></a>
+                                        <a class="button" href="{{ route('blogs.show', ['blog' => $blog]) }}">Read More
+                                            <i class="ti-arrow-right"></i></a>
                                     </div>
                                 </div>
                             @endforeach
